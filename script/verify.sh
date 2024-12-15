aaca12d9e86741428ba2aa074b2b0e018dcf45a8
sshmatrix <cryptosauron@protonmail.com>
2024-12-15 19:59:27 +0530

    Delta Oracle v0.0 meta

--- script/verify.sh
+++ script/verify.sh
@@ -1,1 +1,1 @@
-source .env && forge verify-contract --chain-id 1 --etherscan-api-key $ETHERSCAN_KEY 0x9d79FF2EC00b812E32D9768d88782101cB08D875 src/GatewayManager.sol:GatewayManager
+source .env && forge verify-contract --chain-id 1 --etherscan-api-key $ETHERSCAN_KEY 0x9d79FF2EC00b812E32D9768d88782101cB08D875 src/AntitokenDeltaOracle.sol:AntitokenDeltaOracle
